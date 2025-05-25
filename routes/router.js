1df793e8908cd737163f3f6622989a2ee525200e
ScaxCodes <116537717+ScaxCodes@users.noreply.github.com>
2025-05-25 08:53:51 +0200

    Add signup view

--- routes/router.js
+++ routes/router.js
@@ -5,5 +5,8 @@ const router = Router();
 router.get("/", (req, res) => {
   res.render("index");
 });
+router.get("/signup", (req, res) => {
+  res.render("signup", { errors: [], formData: {} });
+});
 
 module.exports = router;
