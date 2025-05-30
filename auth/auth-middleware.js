0d816217fa524063518f94e98c8006ac103f579e
ScaxCodes <116537717+ScaxCodes@users.noreply.github.com>
2025-05-30 07:52:48 +0200

    Add basic auth middleware

---
+++ auth/auth-middleware.js
@@ -0,0 +1,12 @@
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.status(401).render("unauthorized", {
+    message: "You must be logged in to view this page.",
+  });
+}
+
+module.exports = {
+  ensureAuthenticated,
+};
