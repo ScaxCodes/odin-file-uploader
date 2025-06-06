21f5aeb9adc32829c75638809edf5b5077ad062e
ScaxCodes <116537717+ScaxCodes@users.noreply.github.com>
2025-06-06 22:53:28 +0200

    Add logic to create a folder via folderController.js

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -1,9 +1,24 @@
-function addFolder(req, res) {
+const asyncHandler = require("express-async-handler");
+const prisma = require("../db/prisma");
+
+const addFolder = asyncHandler(async (req, res) => {
   const { name } = req.body;
-  // Logic to add a folder
-  console.log(`Adding folder: ${name}`);
-  res.redirect("/dashboard");
-}
+  const userId = req.user.id;
+
+  try {
+    const folder = await prisma.directory.create({
+      data: {
+        name,
+        userId,
+      },
+    });
+    console.log("New folder created:", folder);
+    res.redirect("/dashboard");
+  } catch (err) {
+    console.error("Error creating folder:", err);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
 module.exports = {
   addFolder,
