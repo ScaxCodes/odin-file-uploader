9cbe83f15d3dab529a39cf89aa4f1a27518120ed
ScaxCodes <116537717+ScaxCodes@users.noreply.github.com>
2025-06-08 21:26:52 +0200

    Handle error when creating a folder that already exists

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -1,19 +1,33 @@
 const asyncHandler = require("express-async-handler");
 const prisma = require("../db/prisma");
+const { getDashboardData } = require("./viewController");
 
 const addFolder = asyncHandler(async (req, res) => {
   const folderName = titleCase(req.body.name.trim());
   const userId = req.user.id;
 
-  const folder = await prisma.directory.create({
-    data: {
-      name: folderName,
-      userId,
-    },
-  });
+  try {
+    const folder = await prisma.directory.create({
+      data: {
+        name: folderName,
+        userId,
+      },
+    });
 
-  console.log("New folder created:", folder);
-  res.redirect("/dashboard?folder=" + folder.id);
+    console.log("New folder created:", folder);
+    res.redirect("/dashboard?folder=" + folder.id);
+  } catch (err) {
+    const folderAlreadyExists =
+      err.code === "P2002" &&
+      err.meta?.target?.includes("name") &&
+      err.meta?.target?.includes("userId");
+    if (folderAlreadyExists) {
+      const data = await getDashboardData(req, "Folder name already exists!");
+      return res.render("dashboard", data);
+    }
+
+    throw err;
+  }
 });
 
 function titleCase(str) {
--- controllers/viewController.js
+++ controllers/viewController.js
@@ -2,18 +2,24 @@ const asyncHandler = require("express-async-handler");
 const prisma = require("../db/prisma");
 
 const renderDashboard = asyncHandler(async (req, res) => {
-  const folders = await getFoldersById(req, res);
+  const data = await getDashboardData(req);
+  res.render("dashboard", data);
+});
+
+async function getDashboardData(req, error = null) {
+  const folders = await getFoldersById(req);
   const selectedFolderId = Number(req.query.folder) || folders[0]?.id;
 
-  res.render("dashboard", {
+  return {
     folders,
     selectedFolderId,
     files: [
       { id: 10, name: "report.pdf" },
       { id: 11, name: "image.png" },
     ],
-  });
-});
+    error,
+  };
+}
 
 async function getFoldersById(req) {
   const userId = req.user.id;
@@ -28,4 +34,5 @@ async function getFoldersById(req) {
 
 module.exports = {
   renderDashboard,
+  getDashboardData,
 };
